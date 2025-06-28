9d44364fd9273a8eceb3824d56ccb007bc7f322a
Ganesh <ganeshbhutham7@gmail.com>
2025-06-28 12:08:56 +0530

    first commit

--- C# Project/Program.cs
+++ C# Project/Program.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+
 
 namespace C__Project
 {
-    using System;
-    using System.Collections.Generic;
+
+
 
     class Student
     {
+     
         public int ID { get; set; }
         public string Name { get; set; }
     }
 
     class Program
     {
-        static List<Student> students = new List<Student>();
+        static string connectionString = @"Server=GANESH;Database=StudentDB;Trusted_Connection=True;";
+
 
         static void Main()
         {
@@ -58,51 +62,102 @@ namespace C__Project
 
         static void AddStudent()
         {
-            Console.Write("Enter Student ID: ");
-            int id = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter Student Name: ");
-            string name = Console.ReadLine();
+            
+                Console.Write("Enter Student ID: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter Student Name: ");
+                string name = Console.ReadLine();
 
-            students.Add(new Student { ID = id, Name = name });
-            Console.WriteLine("Student added successfully!");
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "INSERT INTO Students (ID, Name) VALUES (@ID, @Name)";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@ID", id);
+                    cmd.Parameters.AddWithValue("@Name", name);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+
+                Console.WriteLine("Student added Succesfully.");
         }
 
         static void ViewStudents()
         {
-            Console.WriteLine("\nList of Students:");
-            foreach (var student in students)
-            {
-                Console.WriteLine($"ID: {student.ID}, Name: {student.Name}");
-            }
+            
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT * FROM Students";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Console.WriteLine("\nList of Students:");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"ID: {reader["ID"]}, Name: {reader["Name"]}");
+                    }
+
+                    reader.Close();
+                    conn.Close();
+                }
+
         }
 
         static void SearchStudent()
         {
-            Console.Write("Enter ID to search: ");
-            int id = Convert.ToInt32(Console.ReadLine());
-            var student = students.Find(s => s.ID == id);
+            
+                Console.Write("Enter ID to search: ");
+                int id = Convert.ToInt32(Console.ReadLine());
 
-            if (student != null)
-                Console.WriteLine($"Found: ID = {student.ID}, Name = {student.Name}");
-            else
-                Console.WriteLine("Student not found!");
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT * FROM Students WHERE ID = @ID";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@ID", id);
+
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        Console.WriteLine($"Found: ID = {reader["ID"]}, Name = {reader["Name"]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Student not found!");
+                    }
+
+                    reader.Close();
+                    conn.Close();
+                }
+
         }
 
         static void DeleteStudent()
         {
-            Console.Write("Enter ID to delete: ");
-            int id = Convert.ToInt32(Console.ReadLine());
-            var student = students.Find(s => s.ID == id);
+           
+                Console.Write("Enter ID to delete: ");
+                int id = Convert.ToInt32(Console.ReadLine());
 
-            if (student != null)
-            {
-                students.Remove(student);
-                Console.WriteLine("Student deleted.");
-            }
-            else
-            {
-                Console.WriteLine("Student not found!");
-            }
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "DELETE FROM Students WHERE ID = @ID";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@ID", id);
+
+                    conn.Open();
+                    int rows = cmd.ExecuteNonQuery();
+                    conn.Close();
+
+                    if (rows > 0)
+                        Console.WriteLine("Student deleted.");
+                    else
+                        Console.WriteLine("Student not found!");
+                }
+
         }
     }
 
